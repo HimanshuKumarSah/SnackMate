609df711404ea8c77dd0504055b535b23f449ad4
Himanshu Kumar Sah <himanshu.sah2004@gmail.com>
2024-11-18 00:41:28 +0530

    Created Admin Panel, added Food Add Menu and Food List Display

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,4 +1,4 @@
 import mongoose from 'mongoose';
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://himanshusah2004:himanshusah2004@cluster0.nlzno.mongodb.net/snackmate').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://himanshusah2004:himanshusah2004@cluster0.nlzno.mongodb.net/?').then(()=>console.log("DB Connected"));
 }
