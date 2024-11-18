432649b090bbb3bdbc5a850b50584b0ad577a7a6
Himanshu Kumar Sah <himanshu.sah2004@gmail.com>
2024-11-18 14:01:46 +0530

    Removed backend (rebuilding it)

--- back-end/config/db.js
+++
@@ -1,4 +0,0 @@
-import mongoose from 'mongoose';
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://himanshusah2004:himanshusah2004@cluster0.nlzno.mongodb.net/?').then(()=>console.log("DB Connected"));
-}
